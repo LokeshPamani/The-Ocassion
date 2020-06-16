d09dbe16b1f62790f8b9d49e17f6a52788b68ed3
Saksham Kumar <sakshamk88@gmail.com>
2020-06-16 11:26:13 +0530

    Warnings removed.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Login from './components/Login/Login'
+import React from "react";
+
+import "./App.css";
+import Login from "./components/Login/Login";
 function App() {
   return (
     <div className="App">
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,88 +1,104 @@
-import { Layout, Row, Col, Divider, Typography,Space,Input, Tooltip, Button } from 'antd';
-import React, { Component } from 'react'
-import { InfoCircleOutlined, UserOutlined } from '@ant-design/icons';
+import {
+  Layout,
+  Row,
+  Col,
+  Divider,
+  Typography,
+  Space,
+  Input,
+  Tooltip,
+  Button,
+} from "antd";
+import React, { Component } from "react";
+import { InfoCircleOutlined, UserOutlined } from "@ant-design/icons";
 
-import loginpic from '../../Photos/loginpic.png'
-import './login.css';
-const { Header,  Content } = Layout;
+import loginpic from "../../Photos/loginpic.png";
+import "./login.css";
+const { Header, Content } = Layout;
 const { Title } = Typography;
 
-
-
 class Login extends Component {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-             username: '',
-             password : ''
-        }
-    }
-    
-    handleUsernameInput=val=>{
-        this.setState({
-            username : val.target.value
-        })
-    }
+  constructor(props) {
+    super(props);
 
-    handlePasswordInput=val=>{
-        this.setState({
-            password : val.target.value
-        })
-    }
+    this.state = {
+      username: "",
+      password: "",
+    };
+  }
 
-    handleLogin=()=>{
-        console.log('----state',this.state)
-    }
+  handleUsernameInput = (val) => {
+    this.setState({
+      username: val.target.value,
+    });
+  };
 
-    render() {
-        return (
-            <div>
-                <Layout>
-                    <Header className="header" >
-                        <Title className='title-header' style={{color:"white",padding:'15px'}} level={3}>The Occasion</Title>
-                    </Header>
-                    <Content className='content'>
-                    <Row>
-                        <Col span={11}>
-                        <img id='login-pic' src={loginpic} />
-                        </Col>
-                        <Divider type='vertical' style={{height:'100%'}} />
-                        <Col className='login-title' span={11}>
-                            <Title  level={3}>LogIn</Title>
-                            <Space direction="vertical">
-                                <Input
-                                    className = "input-login"
-                                    onChange={val=>this.handleUsernameInput(val)}
-                                    placeholder="Enter your username"
-                                    prefix={<UserOutlined className="site-form-item-icon" />}
-                                    suffix={
-                                        <Tooltip title="Extra information">
-                                        <InfoCircleOutlined style={{ color: 'rgba(0,0,0,.45)' }} />
-                                        </Tooltip>
-                                    }
-                                    />
-                                    <Input.Password 
-                                        onChange={val=>this.handlePasswordInput(val)}
-                                        className = "input-login"
-                                        placeholder="input password" 
-                                    />
-                                    <Button 
-                                    onClick={this.handleLogin}
-                                    className='header' 
-                                    type="primary" 
-                                    block>
-                                        LogIn
-                                    </Button>
-                            </Space>
-                        </Col>
-                    </Row>
-                    </Content>
-                    
-                </Layout>
-            </div>
-        )
-    }
+  handlePasswordInput = (val) => {
+    this.setState({
+      password: val.target.value,
+    });
+  };
+
+  handleLogin = () => {
+    console.log("----state", this.state);
+  };
+
+  render() {
+    return (
+      <div>
+        <Layout>
+          <Header className="header">
+            <Title
+              className="title-header"
+              style={{ color: "white", padding: "15px" }}
+              level={3}
+            >
+              The Occasion
+            </Title>
+          </Header>
+          <Content className="content">
+            <Row>
+              <Col span={11}>
+                <img id="login-pic" src={loginpic} alt="Something." />
+              </Col>
+              <Divider type="vertical" style={{ height: "100%" }} />
+              <Col className="login-title" span={11}>
+                <Title level={3}>LogIn</Title>
+                <Space direction="vertical">
+                  <Input
+                    className="input-login"
+                    onChange={(val) => this.handleUsernameInput(val)}
+                    placeholder="Enter your username"
+                    prefix={<UserOutlined className="site-form-item-icon" />}
+                    suffix={
+                      <Tooltip title="Extra information">
+                        <InfoCircleOutlined
+                          style={{ color: "rgba(0,0,0,.45)" }}
+                        />
+                      </Tooltip>
+                    }
+                  />
+                  <Input.Password
+                    onChange={(val) => this.handlePasswordInput(val)}
+                    className="input-login"
+                    placeholder="input password"
+                  />
+                  <Button
+                    onClick={this.handleLogin}
+                    className="header"
+                    type="primary"
+                    block
+                  >
+                    LogIn
+                  </Button>
+                </Space>
+              </Col>
+            </Row>
+          </Content>
+        </Layout>
+      </div>
+    );
+  }
 }
 
-export default Login
+export default Login;
