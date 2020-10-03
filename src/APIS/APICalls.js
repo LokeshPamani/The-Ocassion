b6cc1520715853cbe6344f31c2f02d875c8a1c6c
Saksham Kumar <sakshamk88@gmail.com>
2020-10-03 20:32:11 +0530

    ip change

--- src/APIS/APICalls.js
+++ src/APIS/APICalls.js
@@ -1,43 +1,38 @@
-import axios from 'axios'
+import axios from "axios";
 
-const BASEURL="http://localhost:5001/"
+const BASEURL = "http://13.127.121.92:5001/";
 const options = {
   headers: {
-         "Content-Type": "application/json"
-       }}
+    "Content-Type": "application/json",
+  },
+};
 axios.defaults.withCredentials = true;
 
-export const getCall = url => (
-    // fetch(BASEURL+url, {
-    //   method: "GET",
-    //   credentials: 'include'
-    // }).then(res=>res.json())
-    axios.get( BASEURL+url)
-  );
-  
+export const getCall = (url) =>
+  // fetch(BASEURL+url, {
+  //   method: "GET",
+  //   credentials: 'include'
+  // }).then(res=>res.json())
+  axios.get(BASEURL + url);
 
+export const postCall = (url, data) =>
+  // fetch(BASEURL+url, {
+  //   method: "POST",
+  //   body: JSON.stringify(data),
+  //   credentials: 'include',
+  //   headers: {
+  //     "Content-Type": "application/json"
+  //   }
+  // }).then(res=>res.json())
+  axios.post(BASEURL + url, JSON.stringify(data), options);
 
-export const postCall = (url,data) => (
-    // fetch(BASEURL+url, {
-    //   method: "POST",
-    //   body: JSON.stringify(data),
-    //   credentials: 'include',
-    //   headers: {
-    //     "Content-Type": "application/json"
-    //   }
-    // }).then(res=>res.json())
-    axios.post( BASEURL+url,JSON.stringify(data),options)
-  );
-
-  export const putCall = (url,data) => (
-    // fetch(BASEURL+url, {
-    //   method: "POST",
-    //   body: JSON.stringify(data),
-    //   credentials: 'include',
-    //   headers: {
-    //     "Content-Type": "application/json"
-    //   }
-    // }).then(res=>res.json())
-    axios.put( BASEURL+url,JSON.stringify(data),options)
-  );
-  
+export const putCall = (url, data) =>
+  // fetch(BASEURL+url, {
+  //   method: "POST",
+  //   body: JSON.stringify(data),
+  //   credentials: 'include',
+  //   headers: {
+  //     "Content-Type": "application/json"
+  //   }
+  // }).then(res=>res.json())
+  axios.put(BASEURL + url, JSON.stringify(data), options);
--- src/util/Session.js
+++ src/util/Session.js
@@ -1,70 +1,70 @@
-import axios from 'axios'
+import axios from "axios";
 
-const BASEURL="http://localhost:5001"
+const BASEURL = "http://13.127.121.92:5001";
 const options = {
   headers: {
-         "Content-Type": "application/json"
-       }}
+    "Content-Type": "application/json",
+  },
+};
 axios.defaults.withCredentials = true;
 
-export const signup = user => {
-    
-    console.log(user)
-    fetch(BASEURL+"/api/users", {
-      method: "POST",
-      body: JSON.stringify(user),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
+export const signup = (user) => {
+  console.log(user);
+  fetch(BASEURL + "/api/users", {
+    method: "POST",
+    body: JSON.stringify(user),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 };
-  
-export const login = user => (
-    // fetch(BASEURL+"/api/session", {
-    //   method: "POST",
-    //   body: JSON.stringify(user),
-    //   credentials: 'include',
-    //   headers: {
-    //     "Content-Type": "application/json"
-    //   }
-    // })
-    axios.post( BASEURL+"/api/session",JSON.stringify(user),options)
-  );
+
+export const login = (user) =>
+  // fetch(BASEURL+"/api/session", {
+  //   method: "POST",
+  //   body: JSON.stringify(user),
+  //   credentials: 'include',
+  //   headers: {
+  //     "Content-Type": "application/json"
+  //   }
+  // })
+  axios.post(BASEURL + "/api/session", JSON.stringify(user), options);
 
 export const logout = () => {
   //  fetch(BASEURL+"/api/session", { method: "DELETE" ,credentials: 'include'})
   axios.defaults.withCredentials = true;
   //console.log('the url is',url,wmc_tokens)
-  console.log('before the axios')
- return (axios.delete(BASEURL+"/api/session"))
+  console.log("before the axios");
+  return axios.delete(BASEURL + "/api/session");
 };
 
 export const checkLoggedIn = async () => {
-    const response = await fetch(BASEURL+'/api/session',{credentials: 'include'});
-    console.log('response is ',response)
-    let preloadedState;
-    try{
+  const response = await fetch(BASEURL + "/api/session", {
+    credentials: "include",
+  });
+  console.log("response is ", response);
+  let preloadedState;
+  try {
     const result = await response.json();
-    console.log('the out put ',result)
+    console.log("the out put ", result);
     //const {user}=result
-    const {userId , username}=result
-    const user={
-      userId :userId,
-      username : username
-    }
-    console.log('user is ',user)
+    const { userId, username } = result;
+    const user = {
+      userId: userId,
+      username: username,
+    };
+    console.log("user is ", user);
     if (user) {
       preloadedState = {
-        session: user
+        session: user,
       };
     }
-    console.log('preloaded state is in try  ',preloadedState) 
-    }
-    catch{
-      preloadedState = {};
-    }
-     
-    console.log('preloaded state is ',preloadedState) 
-    
-    return preloadedState;
-  };
+    console.log("preloaded state is in try  ", preloadedState);
+  } catch {
+    preloadedState = {};
+  }
+
+  console.log("preloaded state is ", preloadedState);
+
+  return preloadedState;
+};
